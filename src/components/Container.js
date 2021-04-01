cd0498ec900c4f94a8d97556cac6019ae8e8d01d
Cris Franco <cris.franco219@gmail.com>
2021-04-01 17:07:04 -0400

    created component files and css file

---
+++ src/components/Container.js
@@ -0,0 +1,8 @@
+import API from "../utils/API";
+import React, { Component } from "react";
+import SearchBox from "./SearchBox";
+// import SearchBox from "./SearchBox"
+import TableData from "./TableData";
+import "./style.css";
+
+export default Container;
